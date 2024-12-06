a0981c148f2f41ec4d77f288f806171ceaea81b2
Zara <Zara>
2024-12-06 13:40:46 +0100

    yu

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-import alpinejs from "@astrojs/alpinejs";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [alpinejs()]
-});
